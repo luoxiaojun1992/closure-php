56266a10e4788f3db6ba204881a0d49b1bb953b5
luoxiaojun <luoxiaojun1992@sina.cn>
2020-11-26 14:05:22 +0800

    arch

--- src/Compiler/Compiler.php
+++ src/Compiler/Compiler.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace Lxj\ClosurePHP\Compiler;
+
+class Compiler
+{
+    protected $parser;
+
+    public function __construct($parser = null)
+    {
+
+    }
+
+    public function compile($code)
+    {
+
+    }
+
+    public function compilePath($filePath)
+    {
+
+    }
+}
