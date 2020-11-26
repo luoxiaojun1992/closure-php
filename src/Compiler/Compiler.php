0a03c1306d7a8ce6c5ebfbab17d83538033fe1b0
luoxiaojun <luoxiaojun1992@sina.cn>
2020-11-26 18:52:11 +0800

    arch

--- src/Compiler/Compiler.php
+++ src/Compiler/Compiler.php
@@ -6,32 +6,37 @@ class Compiler
 {
     protected $parser;
 
-    public function __construct($parser = null)
-    {
+    protected $transformer;
 
+    public function __construct($parser = null, $transformer = null)
+    {
+        $this->parser = $parser ?: (new \PhpParser\ParserFactory())->create(\PhpParser\ParserFactory::PREFER_PHP7);
+        $this->transformer = $transformer ?: (new Transformer());
     }
 
     public function compile($code)
     {
-
+        $ast = $this->parseAst($code);
+        $transformedAst = $this->transform($ast);
+        return $this->generateCode($transformedAst);
     }
 
     public function compilePath($filePath)
     {
-
+        return $this->compile(file_get_contents($filePath));
     }
 
-    protected function parseAst()
+    protected function parseAst($code)
     {
-
+        return $this->parser->parse($code);
     }
 
     protected function transform($ast)
     {
-
+        return $this->transformer->transform($ast);
     }
 
-    protected function generateCode()
+    protected function generateCode($ast)
     {
 
     }
--- src/Compiler/Transformer.php
+++ src/Compiler/Transformer.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace Lxj\ClosurePHP\Compiler;
+
+class Transformer
+{
+    public function transform($ast)
+    {
+
+    }
+}
