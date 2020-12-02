3c37029cf04e5de283e08b6208c879acbd4433fd
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-02 13:22:06 +0800

    abstract facade

--- src/Sugars/Facade.php
+++ src/Sugars/Facade.php
@@ -2,7 +2,34 @@
 
 namespace Lxj\ClosurePHP\Sugars;
 
+use function Lxj\ClosurePHP\Sugars\Object\callObjectMethod;
+
 abstract class Facade
 {
-    //TODO
+    protected static function getAccessor()
+    {
+        return null;
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed|null
+     * @throws \Exception
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $accessor = static::getAccessor();
+        if ($accessor) {
+            return callObjectMethod(
+                $accessor,
+                $name,
+                Scope::PUBLIC,
+                $arguments,
+                false
+            );
+        }
+
+        return null;
+    }
 }
